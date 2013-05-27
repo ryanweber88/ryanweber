42c91f7288f14accd12d383e45daebe5d6d5db99
Ryan Weber <ryanweber@ryanweber-MXC062.(none)>
2013-05-27 02:24:03 -0400

    More setup

--- main.php
+++ main.php
@@ -5,7 +5,6 @@ $loader = new Twig_Loader_String();
 $loader = new Twig_Loader_Filesystem('/var/www/ryanweber/public_html/XHTML');
 $twig = new Twig_Environment($loader);
 try{
-	echo 'try';
 	echo $twig->render('home.html.twig', array('name' => 'Ryan'));
 } catch (Exception $e){
 	echo_r($e);
